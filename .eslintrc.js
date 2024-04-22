c5e9a1624063c43eee2bbdfc80a5cb40210ce1e3
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-04-22 22:26:04 +0300

    Update .eslintrc.js
    ESlinting has been configured in .eslintrc.js to check syntax, find problems and enforce coding style for Javascript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,38 @@
-module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-  }
-}
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const pinoLogger = require('./logger')
+const path = require('path')
+const connectToDatabase = require('./models/db')
+const app = express()
+app.use('*', cors())
+const port = 3060
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+  pinoLogger.info('Connected to DB')
+})
+  .catch((e) => console.error('Failed to connect to DB', e))
+app.use(express.json())
+// Route files
+const secondChanceRoutes = require('./routes/secondChanceItemsRoutes')
+const authRoutes = require('./routes/authRoutes')
+const searchRoutes = require('./routes/searchRoutes')
+const pinoHttp = require('pino-http')
+const logger = require('./logger')
+app.use(pinoHttp({ logger }))
+app.use(express.static(path.join(__dirname, 'public')))
+// Use Routes
+app.use('/api/secondchance/items', secondChanceRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/secondchance/search', searchRoutes)
+// Global Error Handler
+app.use((err, req, res) => {
+  console.error(err)
+  res.status(500).send('Internal Server Error')
+})
+app.get('/', (req, res) => {
+  res.send('Inside the server')
+})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
